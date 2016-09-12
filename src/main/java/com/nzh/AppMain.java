e77ee94d1995777b7704f1ab2bcf0662614e9def
牛志欢 <niuzhihuan@funi.com>
2016-09-12 19:57:26 +0800

    窗体

---
+++ src/main/java/com/nzh/AppMain.java
@@ -0,0 +1,11 @@
+package com.nzh;
+
+/**
+ * 工具应用程序
+ * @author zhihuan.niu
+ */
+public class AppMain {
+    public static void main( String[] args ){
+        MainFrame frame=new MainFrame();
+    }
+}
